994ff4a5f6c9aaba414e18474125ce911be1b260
Salma <salmaelaidy@aucegypt.edu>
2025-04-04 12:02:09 +0200

    Updated random number generation

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,7 @@
 #include "calculator.h"
 #include <iostream>
 #include <cstdlib>
+#include <random>
 
 int add(int a, int b) { return a + b; }
 int subtract(int a, int b) { return a - b; }
@@ -9,5 +10,9 @@ double divide(int a, int b) { return (b != 0) ? (double)a / b : 0; }
 long factorial(int n) { return (n <= 1) ? 1 : n * factorial(n - 1); }
 int gcd(int a, int b) { return (b == 0) ? a : gcd(b, a % b); }
 int lcm(int a, int b) { return (a * b) / gcd(a, b); }
-int randomInRange(int min, int max) { return rand() % (max - min + 1) + min; }
-
+int randomInRange(int min, int max) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(min, max);
+    return dis(gen);
+}
